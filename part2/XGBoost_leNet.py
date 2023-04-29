0d8682e4f3a98a5145e1dfb3ab6f6d110dadf63a
HR-Chaos <91175075+HR-Chaos@users.noreply.github.com>
2023-04-29 10:39:07 -0700

    Add files via upload

---
+++ part2/XGBoost_leNet.py
@@ -0,0 +1,105 @@
+import numpy as np
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, confusion_matrix
+from joblib import dump, load
+import scipy.io
+import xgboost as xgb
+
+print('\n\n#------------------------------------------------------------\n')
+mat_data = scipy.io.loadmat('MNIST-LeNet5.mat')
+print('keys -> ', mat_data.keys())
+
+X = mat_data['train_fea']
+Y = mat_data['train_gnd']
+Y = Y - 1
+
+test_X = mat_data['test_fea']
+test_Y = mat_data['test_gnd']
+test_Y -= 1
+
+print('train data shape -> ', X.shape)
+print('test data shape -> ', test_X.shape)
+print('train labels shape -> ', Y.shape)
+print('test labels shape -> ', test_Y.shape)
+
+#------------------------------------------------------------
+max_depth = [2, 4, 6, 8, 10, 20]
+eta_vals = [0.01, 0.05, 0.1, 0.15, 0.2, 0.5, 1, 2, 5, 10]
+n_est = [1, 5, 10, 20, 50, 100]
+best_acc = 0
+best_params = {}
+best_model = None
+for depth in max_depth:
+    for et in eta_vals:
+        for n in n_est:
+            params = {'max_depth': depth,
+                      'eta': et, #learning rate
+                      'n_estimators': n, #number of trees
+                      'objective': 'multi:softmax', 
+                      'num_class': 10,
+                      'verbosity': 0,
+                      'tree_method': 'gpu_hist'
+                      }
+            clf = xgb.XGBClassifier(**params)
+            clf.fit(X, Y)
+            y_pred = clf.predict(test_X)
+            accuracy = accuracy_score(test_Y, y_pred)
+            if accuracy > best_acc:
+                best_acc = accuracy
+                best_params = params
+                best_model = clf
+            print(f"max_depth: {depth}, eta: {et}, n_estimators: {n}, Accuracy: {accuracy}")
+            
+# params = {'max_depth': 2,
+#           'eta': 1, #learning rate
+#           'n_estimators': 100, #number of trees
+#           'objective': 'multi:softmax', 
+#           'num_class': 10
+#           }
+
+# #------------------------------------------------------------
+# clf = xgb.XGBClassifier(**params)
+# clf.fit(X, Y)
+
+# # Evaluate the performance of the classifier on the testing data
+# y_pred = clf.predict(test_X)
+# accuracy = accuracy_score(test_Y, y_pred)
+# print(f"Accuracy: {accuracy}")
+
+# # Plot the first 10 images from the test set
+# shift = 0
+# fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(8, 4))
+# for i, ax in enumerate(axes.flat):
+#     if i < 10:
+#         ax.imshow(test_X[i+shift].reshape(28, 28), cmap='gray')
+#         ax.set_xlabel(f"Label: {test_Y[i+shift]+1}, pred: {y_pred[i+shift]+1}")
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+# plt.tight_layout()
+# plt.show()
+
+# plot the confusion matrix
+cm = confusion_matrix(test_Y, y_pred)
+fig, ax = plt.subplots()
+im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+ax.figure.colorbar(im, ax=ax)
+ax.set(xticks=np.arange(cm.shape[1]),
+       yticks=np.arange(cm.shape[0]),
+       xticklabels=np.arange(10),
+       yticklabels=np.arange(10),
+       xlabel='Predicted label',
+       ylabel='True label')
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+fmt = '.0f' 
+thresh = cm.max() / 2.
+for i in range(cm.shape[0]):
+    for j in range(cm.shape[1]):
+        ax.text(j, i, format(cm[i, j], fmt),
+                ha="center", va="center",
+                color="white" if cm[i, j] > thresh else "black")
+fig.tight_layout()
+plt.show()
+
+print('\n#------------------------------------------------------------\n\n')
